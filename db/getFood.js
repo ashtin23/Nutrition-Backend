76dc00ed410e4c495e63804e49cc8d3c873f1180
ashtinlatting <rlatting92@gwu.edu>
2019-09-10 10:54:50 -0400

    backend mostly done

--- db/getFood.js
+++ db/getFood.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 
 axios
 .get(
-  "https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&max=5000&api_key=yA4dQlWhts5H48QIOFEWJX9ZcbkLtWE9fC4qLukR"
+  "https://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&max=1400&api_key=yA4dQlWhts5H48QIOFEWJX9ZcbkLtWE9fC4qLukR"
 )
 .then(res => {
   // handle success
   console.log(res.data);
   let stringified = JSON.stringify(res.data);
-  fs.writeFile(__dirname + "/test.json", stringified, "utf8", err => {
+  fs.writeFile(__dirname + "/food.json", stringified, "utf8", err => {
     if (err) {
       console.error(err);
     } else {
